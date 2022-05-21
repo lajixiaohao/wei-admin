006cc7371bd32f303e5d45e7a33ba9776351647d
lajixiaohao <2176974730@qq.com>
2022-05-21 21:55:15 +0800

    modify const.js

--- src/common/const.js
+++ src/common/const.js
@@ -1,6 +1,6 @@
-// 上传图片地址
+// 图片上传接口
 export const UPLOAD_IMAGE_URL = process.env.VUE_APP_BASE_API + '/upload/image'
-// 上传视频地址
+// 视频上传接口
 export const UPLOAD_VIDEO_URL = process.env.VUE_APP_BASE_API + '/upload/video'
-// 上传附件地址
+// 附件上传接口
 export const UPLOAD_ATTACHMENT_URL = process.env.VUE_APP_BASE_API + '/upload/attachment'
